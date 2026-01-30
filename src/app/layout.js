22af99a4a75c7fa1ad279848de9be907dde4dcbd
debrajdeb137 <debrajdeb137@gmail.com>
2026-01-30 14:14:29 +0530

    Add Local SEO Hotel schema

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,11 +30,56 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+        <Navbar />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Hotel",
+              "name": "Paarvi Palace",
+              "url": "https://paarvipalace.com",
+              "logo": "https://paarvipalace.com/logo/logo.png",
+              "image": [
+                "https://paarvipalace.com/images/rooms/deluxe/1.jpeg",
+                "https://paarvipalace.com/images/rooms/executive/1.jpeg",
+                "https://paarvipalace.com/images/rooms/family/1.jpeg"
+              ],
+              "description": "Paarvi Palace is a peaceful luxury homestay near Ram Mandir, Ayodhya offering Deluxe, Executive and Family Suite rooms.",
+              "priceRange": "₹₹",
+              "telephone": "+91-9250538645",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Bhiki Ka Purva, Ashapur Village, Darshan Nagar, Ranopali",
+                "addressLocality": "Ayodhya",
+                "addressRegion": "Uttar Pradesh",
+                "postalCode": "224123",
+                "addressCountry": "IN"
+              },
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": 26.7956,
+                "longitude": 82.1943
+              },
+              "amenityFeature": [
+                { "@type": "LocationFeatureSpecification", "name": "Free Wi-Fi" },
+                { "@type": "LocationFeatureSpecification", "name": "Air Conditioning" },
+                { "@type": "LocationFeatureSpecification", "name": "Room Service" },
+                { "@type": "LocationFeatureSpecification", "name": "Family Friendly" }
+              ],
+              "checkinTime": "12:00",
+              "checkoutTime": "11:00",
+              "sameAs": [
+                "https://www.google.com/maps"
+              ]
+            }),
+          }}
+        />
+
         {children}
         {/* <BottomBookingForm/> */}
-        <BookingSection/>
-        <Footer/>
+        <BookingSection />
+        <Footer />
       </body>
     </html>
   );

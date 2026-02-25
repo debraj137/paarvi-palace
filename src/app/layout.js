7dfbb05ed64145d3cbe91fccb9151fd2b3cbc2a8
debrajdeb137 <debrajdeb137@gmail.com>
2026-02-25 21:14:59 +0530

    added google analytics code

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import BookingSection from "@/components/BookingSection";
+import Script from "next/script";
 // import BottomBookingForm from "@/components/BottomBookingForm";
 
 const geistSans = Geist({
@@ -60,6 +61,19 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* ✅ Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-634NGVQGS2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-634NGVQGS2');
+          `}
+        </Script>
         <Navbar />
         <script
           type="application/ld+json"

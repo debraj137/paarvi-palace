6da73f9c3c14259ac9432b1f196266f141b8765a
debrajdeb137 <debrajdeb137@gmail.com>
2026-03-05 00:25:30 +0530

    moved pixel code in head

--- src/app/layout.js
+++ src/app/layout.js
@@ -58,6 +58,26 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="beforeInteractive"
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1241403251416000');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -75,7 +95,7 @@ export default function RootLayout({ children }) {
           `}
         </Script>
 
-        <Script id="meta-pixel" strategy="afterInteractive">
+        {/* <Script id="meta-pixel" strategy="afterInteractive">
           {`
   !function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -88,7 +108,7 @@ export default function RootLayout({ children }) {
   fbq('init', '1241403251416000');
   fbq('track', 'PageView');
 `}
-        </Script>
+        </Script> */}
 
         <noscript>
           <img
